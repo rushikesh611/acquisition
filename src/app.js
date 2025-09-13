a428fae6d28d66dc3ba8507aeb56dceb871b2e1a
Hrushikesh Jadhav <rushi611@gmail.com>
2025-09-14 01:52:43 +0530

    actions

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import usersRoutes from '#routes/users.routes.js';
 import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
@@ -43,5 +44,10 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 export default app;
--- src/config/arcjet.js
+++ src/config/arcjet.js
@@ -4,10 +4,10 @@ const aj = arcjet({
   key: process.env.ARCJET_KEY,
   rules: [
     shield({ mode: 'LIVE' }),
-    detectBot({
-      mode: 'LIVE',
-      allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW'],
-    }),
+    // detectBot({
+    //   mode: 'LIVE',
+    //   allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW'],
+    // }),
     slidingWindow({
       mode: 'LIVE',
       interval: '2s',
